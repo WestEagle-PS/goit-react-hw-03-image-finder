4c2abf03f4ef1bcdf147db0349ab396b2f85441c
Anton Orlov <anton_orlov@ukr.net>
2023-02-23 18:07:44 +0200

    Update Loader.jsx

--- src/shared/components/Loader/Loader.jsx
+++ src/shared/components/Loader/Loader.jsx
@@ -3,13 +3,13 @@ import { ProgressBar } from 'react-loader-spinner';
 const Spinner = () => {
   return (
     <ProgressBar
-      height="120"
-      width="120"
+      height="50vh"
+      width="50vw"
       ariaLabel="progress-bar-loading"
       wrapperStyle={{}}
       wrapperClass="progress-bar-wrapper"
       borderColor="#F4442E"
-      barColor="#9cff51"
+      barColor="blue"
     />
   );
 };
